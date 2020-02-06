4d489183dd5734e9301568cbfa4234c28c9bd179
anferasu <andresfeliperamos@hotmail.com>
2020-02-06 13:15:53 +0100

    crear el modelo Estudiantes con la herrameinta gii

--- m3/u1/yii/ejemplos/ejemployii/config/db.php
+++ m3/u1/yii/ejemplos/ejemployii/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=estudiantes',
+    'dsn' => 'mysql:host=localhost;dbname=alumnos',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
---
+++ m3/u1/yii/ejemplos/ejemployii/models/Estudiantes.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "estudiantes".
+ *
+ * @property int $id
+ * @property string $nombre
+ * @property string $apellidos
+ * @property int $edad
+ */
+class Estudiantes extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'estudiantes';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre', 'apellidos', 'edad'], 'required'],
+            [['edad'], 'integer'],
+            [['nombre', 'apellidos'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'apellidos' => 'Apellidos',
+            'edad' => 'Edad',
+        ];
+    }
+}
